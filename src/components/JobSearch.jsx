eff6e59a62821cd5519919c278ee67ad61ac8f5f
Shmuel <semyki@edu.hac.ac.il>
2025-04-06 08:02:33 +0300

    Update website changes

--- src/components/JobSearch.jsx
+++ src/components/JobSearch.jsx
@@ -1,7 +1,7 @@
 // src/components/JobSearch.jsx
 import React, { useState } from 'react';
 import axios from 'axios';
-import './JobSearch.css'; // אם יש לך קובץ CSS לעיצוב
+import './JobSearch.css'; // שמור את העיצוב הקיים
 
 const JobSearch = () => {
   const [keyword, setKeyword] = useState('');
